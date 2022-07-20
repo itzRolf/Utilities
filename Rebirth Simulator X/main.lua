4f8a7dd8d13e26271767556322cb2b4ba83d4296
itzRolf <109205676+itzRolf@users.noreply.github.com>
2022-07-21 00:39:08 +0300

    update

--- Rebirth Simulator X/main.lua
+++ Rebirth Simulator X/main.lua
@@ -11,6 +11,8 @@ local MiscTab = MainWindow:Tab("Misc")
 
 local AutoRebirthSection = MainTab:Section("AutoRebirth")
 local AutoDarkRebirthSection = MainTab:Section("Auto Dark Rebirth")
+local AutoUpgradeSection = MainTab:Section("Auto Upgrade")
+local AutoClaimSection = MainTab:Section("Auto Claim Chest")
 local TeleportSection = TeleportTab:Section("Teleport")
 local MiscSection = MiscTab:Section("Misc")
 
@@ -92,6 +94,51 @@ local ToggleAutoDarkRebirth = AutoDarkRebirthSection:Toggle("Auto Dark Rebirth",
     end
 end)
 
+local ToggleAutoUpgradeDobT = AutoUpgradeSection:Toggle("AutoUpgrade More Double Tokens Chances", false,"Toggle", function(q)
+    getgenv().autoupgradedt = q
+    while true do
+        if getgenv().autoupgradedt == false then return end
+        game:GetService("ReplicatedStorage").Events.Upgrade:FireServer("Upgrade1")
+        wait(5)
+    end
+end)
+
+local ToggleAutoUpgradeDobR = AutoUpgradeSection:Toggle("AutoUpgrade More Double Rebirths Chances", false,"Toggle", function(j)
+    getgenv().autoupgradedr = j
+    while true do
+        if getgenv().autoupgradedr == false then return end
+        game:GetService("ReplicatedStorage").Events.Upgrade:FireServer("Upgrade2")
+        wait(5)
+    end
+end)
+
+local ToggleAutoUpgradeDobTc = AutoUpgradeSection:Toggle("AutoUpgrade More Token Chances", false,"Toggle", function(c)
+    getgenv().autoupgradedtc = c
+    while true do
+        if getgenv().autoupgradedtc == false then return end
+        game:GetService("ReplicatedStorage").Events.Upgrade:FireServer("Upgrade3")
+        wait(5)
+    end
+end)
+
+local ToggleAutoUpgradeMT = AutoUpgradeSection:Toggle("AutoUpgrade More Tokens", false,"Toggle", function(t)
+    getgenv().autoupgrademt = t
+    while true do
+        if getgenv().autoupgrademt == false then return end
+        game:GetService("ReplicatedStorage").Events.Upgrade:FireServer("Upgrade10")
+        wait(5)
+    end
+end)
+
+local ToggleAutoUpgradeArm = AutoUpgradeSection:Toggle("AutoUpgrade All Rebirths Multiplier", false,"Toggle", function(m)
+    getgenv().autoupgradearm = m
+    while true do
+        if getgenv().autoupgradearm == false then return end
+        game:GetService("ReplicatedStorage").Events.Upgrade:FireServer("Upgrade12")
+        wait(5)
+    end
+end)
+
 TeleportSection:Button("Teleport to Spawn", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-13, 15.0245419, -53, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 end)
@@ -129,19 +176,14 @@ TeleportSection:Button("Teleport to Beach", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(391.071045, 25.0245419, 1359.34998, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 end)
 
-
 TeleportSection:Button("Teleport to Alien", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-25.9289551, 19.0245419, -1241.65002, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 end)
 
-
-local CurrentSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
-local CurrentJump = game.Players.LocalPlayer.Character.Humanoid.JumpPower
-
-local SpeedSlider = MiscSection:Slider("Walk Speed", CurrentSpeed, 500 , 10 , 1 ,"Slider", function(f)
+local SpeedSlider = MiscSection:Slider("Walk Speed", 25, 250 , 16 , 1 ,"Slider", function(f)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed  = f
   end)
 
-local JumpSlider = MiscSection:Slider("Jump Power", CurrentJump, 500, 10 , 1,"Slider", function(p)
+local JumpSlider = MiscSection:Slider("Jump Power", 65, 250, 50 , 1, "Slider", function(p)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower  = p
 end)
