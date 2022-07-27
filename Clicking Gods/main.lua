d14167b81121a044efc90fe7ef89541e9070f1bb
itzRolf <109205676+itzRolf@users.noreply.github.com>
2022-07-27 14:11:20 +0300

    trash update

--- Clicking Gods/main.lua
+++ Clicking Gods/main.lua
@@ -8,8 +8,18 @@ local MainWindow = Library:New({
 local MainTab = MainWindow:Tab("Main")
 local MiscTab = MainWindow:Tab("Misc")
 
-local AutoClickSection = MainTab:Section("AutoFarm")
+local AutoClickSection = MainTab:Section("Auto Click")
+local PetsSection = MainTab:Section("Pets")
 local MiscSection = MiscTab:Section("Misc")
+
+local GetEggs = game:GetService("Workspace").Eggs:GetChildren()
+local AllEggs = {}
+local CurrentEgg
+
+for i = 1, #GetEggs do
+  local ThisEgg = GetEggs[i].Name
+  AllEggs[i] = ThisEgg
+end
 
 local ToggleAutoClick = AutoClickSection:Toggle("Auto Click", false,"Toggle", function(k)
     getgenv().autoclick = k
@@ -20,13 +30,32 @@ local ToggleAutoClick = AutoClickSection:Toggle("Auto Click", false,"Toggle", fu
     end
 end)
 
-local CurrentSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
-local CurrentJump = game.Players.LocalPlayer.Character.Humanoid.JumpPower
+local Eggs = PetsSection:Dropdown("Eggs", AllEggs,"","Dropdown", function(segg)
+  CurrentEgg = segg
+end)
 
-local SpeedSlider = MiscSection:Slider("Walk Speed", CurrentSpeed, 500 , 10 , 1 ,"Slider", function(f)
+local ToggleAutoEggTriple = PetsSection:Toggle("Auto Egg Triple", false,"Toggle", function(egt)
+  getgenv().autoeggtriple = egt
+  while true do
+      if getgenv().autoeggtriple == false then return end
+      game:GetService("ReplicatedStorage").RemoteEvents.EggOpened:InvokeServer(CurrentEgg, "Triple")
+      wait(0.5)
+  end
+end)
+
+local ToggleAutoEggSingle = PetsSection:Toggle("Auto Egg Single", false,"Toggle", function(eg)
+  getgenv().autoegg = eg
+  while true do
+      if getgenv().autoegg == false then return end
+      game:GetService("ReplicatedStorage").RemoteEvents.EggOpened:InvokeServer(CurrentEgg, "Single")
+      wait(0.5)
+  end
+end)
+
+local SpeedSlider = MiscSection:Slider("Walk Speed", 25, 500 , 25 , 1 ,"Slider", function(f)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed  = f
   end)
 
-local JumpSlider = MiscSection:Slider("Jump Power", CurrentJump, 500, 10 , 1,"Slider", function(p)
+local JumpSlider = MiscSection:Slider("Jump Power", 50, 500, 50 , 1,"Slider", function(p)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower  = p
 end)
